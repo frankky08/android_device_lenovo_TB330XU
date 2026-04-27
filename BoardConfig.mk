02739f2bcc23f9ad0ef87081d7db8479aa21dd27
frankky08 <frankky.dpr@gmail.com>
2026-04-27 17:44:45 +0700

    Update DEVICE_PATH for Lenovo device configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/lenovo/barley
+DEVICE_PATH := device/lenovo/TB330XU
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
